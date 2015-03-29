48e9ebf86ee79bcd1a314206fefd7528b0cd152c
admin <admin@admindeMacBook-Pro.local>
2015-03-29 15:51:13 +0800

    last

--- idea_list.js
+++ idea_list.js
@@ -1,19 +1,38 @@
+var Ideas = new Mongo.Collection('ideas');
+
+
+
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+  //Session.setDefault('counter', 0);
+
 
   Template.hello.helpers({
     counter: function () {
-      return Session.get('counter');
+      return Ideas.findOne().score;
     }
+    ,
+all_ideas: function () {
+return Ideas.find();
+}
   });
 
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      //Session.set('counter', Session.get('counter') + 1);
+        //Ideas.insert({score:0});
+
+
+        Ideas.update("ft5DYs2LtRBg9goRD", {$set: {score:Ideas.findOne().score+1}});
+
+        
+
     }
   });
+
+
 }
 
 if (Meteor.isServer) {
@@ -21,3 +40,5 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+
